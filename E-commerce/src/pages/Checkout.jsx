7515ff014e236f547a7e8182be6dead9e254ba49
pavit <pavit2000@gmail.com>
2025-06-01 10:58:52 -0600

    Auth update

--- E-commerce/src/pages/Checkout.jsx
+++ E-commerce/src/pages/Checkout.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useCart } from "../context/CartContext";
 import "../CSS/checkout.css";
 import CartBar from "../components/CartBar";
 import QuantityControls from "../components/QuantityControls";
+import { useAuth } from "../context/AuthContext";
 
 function Checkout({ handleCheckout }) {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    }
+  }, [user, navigate]);
   const { cartItems, addToCart, decreaseQuantity, totalQuantity, cartLoading, totalPrice, deleteCart } = useCart();
 
   return (
